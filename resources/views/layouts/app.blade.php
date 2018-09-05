9a37f1a9d9d0a280e5ba1f1c1d1b33ab31f5c975
vagrant <vagrant@homestead>
2018-09-05 04:41:03 +0000

    タイトル変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,10 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <!--
     <title>{{ config('app.name', 'Laravel') }}</title>
+    -->
+    <title>Eitan</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Eitan</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -78,6 +78,9 @@
             @endif
 
             <div class="content">
+                <div>
+                    英単語学習サイト
+                </div>
                 <div class="title m-b-md">
                     Eitan
                 </div>
