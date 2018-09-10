cff0002f48b912040376e8c12e75b31301d7978f
vagrant <vagrant@homestead>
2018-09-10 07:21:53 +0000

    パスワードリセット機能追加

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'notice@eitan.sakura.ne.jp'),
+        'name' => env('MAIL_FROM_NAME', 'Eitango'),
     ],
 
     /*
